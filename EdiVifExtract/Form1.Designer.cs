238f19c351f93d09d929330fe83bf7a361fba27d
Jean-Christophe BILLARD <jchristophe.billard@outlook.com>
2017-04-07 10:55:11 +0200

    commit

---
+++ EdiVifExtract/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EdiVifExtract
+{
+    partial class FormExtract
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerer = new System.Windows.Forms.Button();
+            this.comboBoxSource = new System.Windows.Forms.ComboBox();
+            this.comboBoxDestination = new System.Windows.Forms.ComboBox();
+            this.comboBoxDepot = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonGenerer
+            // 
+            this.buttonGenerer.Location = new System.Drawing.Point(36, 213);
+            this.buttonGenerer.Name = "buttonGenerer";
+            this.buttonGenerer.Size = new System.Drawing.Size(87, 23);
+            this.buttonGenerer.TabIndex = 0;
+            this.buttonGenerer.Text = "genererFichiers";
+            this.buttonGenerer.UseVisualStyleBackColor = true;
+            this.buttonGenerer.Click += new System.EventHandler(this.buttonGenerer_Click);
+            // 
+            // comboBoxSource
+            // 
+            this.comboBoxSource.FormattingEnabled = true;
+            this.comboBoxSource.Location = new System.Drawing.Point(21, 35);
+            this.comboBoxSource.Name = "comboBoxSource";
+            this.comboBoxSource.Size = new System.Drawing.Size(271, 21);
+            this.comboBoxSource.TabIndex = 1;
+            // 
+            // comboBoxDestination
+            // 
+            this.comboBoxDestination.FormattingEnabled = true;
+            this.comboBoxDestination.Location = new System.Drawing.Point(21, 81);
+            this.comboBoxDestination.Name = "comboBoxDestination";
+            this.comboBoxDestination.Size = new System.Drawing.Size(271, 21);
+            this.comboBoxDestination.TabIndex = 2;
+            // 
+            // comboBoxDepot
+            // 
+            this.comboBoxDepot.FormattingEnabled = true;
+            this.comboBoxDepot.Location = new System.Drawing.Point(21, 125);
+            this.comboBoxDepot.Name = "comboBoxDepot";
+            this.comboBoxDepot.Size = new System.Drawing.Size(271, 21);
+            this.comboBoxDepot.TabIndex = 3;
+            // 
+            // FormExtract
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 261);
+            this.Controls.Add(this.comboBoxDepot);
+            this.Controls.Add(this.comboBoxDestination);
+            this.Controls.Add(this.comboBoxSource);
+            this.Controls.Add(this.buttonGenerer);
+            this.Name = "FormExtract";
+            this.Text = "ASCGenerator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormExtract_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerer;
+        private System.Windows.Forms.ComboBox comboBoxSource;
+        private System.Windows.Forms.ComboBox comboBoxDestination;
+        private System.Windows.Forms.ComboBox comboBoxDepot;
+    }
+}
+
---
+++ EdiVifExtract/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using ExtendedXmlSerialization;
+
+namespace EdiVifExtract
+{
+
+    public partial class FormExtract : Form
+    {
+        // Les variables globals au formulaire
+        string appDataArterris = "";//c'est dans ce repertoire qu'on a les droits et qu'il convient d'écrire
+        string appdata = "";//son ss rep.
+        String xml;
+
+        static List<FileInfo> listeFichiers = new List<FileInfo>();
+
+        Dictionary<string, string> DicdepotDirectory = new Dictionary<string,string>();
+        Dictionary<string, string> DicSourceDirectory = new Dictionary<string, string>();
+        Dictionary<string, string> DicdestinationDirectory = new Dictionary<string, string>();
+        static configObject co = new configObject();
+
+        public FormExtract()
+        {
+            DicSourceDirectory.Add("1", @"c:\temp\");
+            DicSourceDirectory.Add("2", @"d:\temp\");
+            DicSourceDirectory.Add("3", @"e:\temp\");
+            DicdestinationDirectory.Add("1", @"c:\1");
+            DicdestinationDirectory.Add("2", @"c:\2");
+            DicdestinationDirectory.Add("3", @"c:\3");
+            DicdepotDirectory.Add("1", @"c:\1");
+
+            co.DicSourceDirectory = DicSourceDirectory;
+            co.DicdestinationDirectory = DicdestinationDirectory;
+            co.DicdepotDirectory = DicdepotDirectory;
+
+            InitializeComponent();
+            appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDataArterris = Path.Combine(appdata, "Arterris");
+            
+
+            if (!Directory.Exists(appDataArterris))
+                Directory.CreateDirectory(appDataArterris);
+
+           
+            ExtendedXmlSerializer xs = new ExtendedXmlSerializer();//pour serialiser en XML la config (sauvegarde des paths src et dst)
+            if (!File.Exists(appDataArterris + "\\configEDI.xml"))//si le fichier n'existe pas on le cré avec init à "";
+            {
+
+                using (StreamWriter wr = new StreamWriter(appDataArterris + "\\configEDI.xml"))
+                {
+                  
+                    xml  = xs.Serialize(co);
+                    wr.WriteLine(xml);
+                }
+
+            }
+
+            //init des txtbox avec les params enregistres dans le xml
+            using (StreamReader rd = new StreamReader(appDataArterris + "\\configEDI.xml"))
+            {
+
+                xml = rd.ReadToEnd();
+                co = xs.Deserialize<configObject>(xml);// as configObject;
+                this.DicdestinationDirectory = co.DicdestinationDirectory;
+                this.DicSourceDirectory = co.DicSourceDirectory;
+                this.DicdepotDirectory = co.DicdepotDirectory;
+               
+            }
+
+            remplirCombo();
+        }
+
+        public void remplirCombo()
+        {
+
+            comboBoxSource.DataSource = new BindingSource(DicSourceDirectory, null);
+            comboBoxSource.DisplayMember = "Value";
+            comboBoxSource.ValueMember = "Key";
+            //-----------------------------------------
+            comboBoxDestination.DataSource = new BindingSource(DicdestinationDirectory, null);
+            comboBoxDestination.DisplayMember = "Value";
+            comboBoxDestination.ValueMember = "key";
+            //-------------------------------------------
+            comboBoxDepot.DataSource = new BindingSource(DicdepotDirectory, null);
+            comboBoxDepot.DisplayMember = "Value";
+            comboBoxDepot.ValueMember = "key";
+
+
+        }
+
+        private void buttonGenerer_Click(object sender, EventArgs e)
+        {
+
+            string[] OriginaleLines = System.IO.File.ReadAllLines(@"c:\temp\10474434.asc");
+            string[] modifiedLines = new string[OriginaleLines.Length];
+            string[] StrSplit = OriginaleLines[0].Split('"');
+            String numEDI = StrSplit[5];
+            numEDI = numEDI.Trim(new Char[] { ' ', '"'});
+            string ediDir = Path.Combine(((KeyValuePair<string, string>)comboBoxSource.SelectedItem).Value, numEDI);
+            Directory.CreateDirectory(ediDir);
+
+
+            int i = 0;
+            foreach (string line in OriginaleLines)
+            {
+                modifiedLines[i++] = line.Remove(0, 3);
+                modifiedLines[i - 1] += Environment.NewLine;
+            }
+
+
+            //le fichier des entetes
+            System.IO.File.WriteAllText(Path.Combine(ediDir,"cde_ent.asc"), modifiedLines[0].ToString());
+
+            String destFile = Path.Combine(ediDir, "cde_lig.asc");
+            if (File.Exists(destFile)) { File.Delete(destFile); }
+
+            for (int j = 1; j < modifiedLines.Length; j++)
+            {
+                System.IO.File.AppendAllText(destFile, modifiedLines[j].ToString());
+            }
+
+            using (System.IO.FileStream fs = System.IO.File.Create(Path.Combine(ediDir, "tpcde")))
+            {
+                fs.Close();
+                fs.Dispose();
+            }
+
+                string value = ((KeyValuePair<string, string>)comboBoxSource.SelectedItem).Value;
+            MessageBox.Show(value);
+
+        }
+
+
+
+        public void creatXML()
+        {
+
+            try
+            {
+                co.DicdestinationDirectory = this.DicdestinationDirectory;
+                co.DicSourceDirectory = this.DicSourceDirectory;
+                co.DicdepotDirectory = this.DicdepotDirectory;
+                ExtendedXmlSerializer xs = new ExtendedXmlSerializer();
+
+                var xml = xs.Serialize(co);
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Erreur lors de la sauvegarde des paramètres: " + e.StackTrace.ToString());
+            }
+
+        }
+
+        private void FormExtract_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            creatXML();
+        }
+    }
+
+    public class configObject
+    {
+        public configObject()
+        {
+
+            //DicdestinationDirectory = new Dictionary<string, string>();//
+            //DicSourceDirectory = new Dictionary<string, string>();
+            //DicdepotDirectory = new Dictionary<string, string>();
+
+        }
+
+        public Dictionary<string, string> DicSourceDirectory;
+        public Dictionary<string, string> DicdestinationDirectory;
+        public Dictionary<string, string> DicdepotDirectory;
+    }
+
+    public class MyObject
+    {
+        public MyObject()
+        {
+
+            //DicdestinationDirectory = new Dictionary<string, string>();//
+            //DicSourceDirectory = new Dictionary<string, string>();
+            //DicdepotDirectory = new Dictionary<string, string>();
+
+        }
+
+        private int Myint;
+
+        public int Myint1 { get => Myint; set => Myint = value; }
+    }
+}
